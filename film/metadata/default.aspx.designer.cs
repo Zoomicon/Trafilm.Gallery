63a0069f9a86ce4b237618ee29eb20df7c3749b4
George Birbilis <GitHub1@zoomicon.com>
2016-05-12 14:20:07 +0300

    - Can now select an item from queryString - Added Scenes and Utterances lists at end of Film and Scene metadata pages - Fixed DisplayMetadata at scene (had wrong format constants) - Fixed Query String based navigation

--- film/metadata/default.aspx.designer.cs
+++ film/metadata/default.aspx.designer.cs
@@ -265,6 +265,15 @@ namespace Trafilm.Gallery {
         protected global::System.Web.UI.WebControls.Label lblScenesDuration;
         
         /// <summary>
+        /// repeaterScenes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater repeaterScenes;
+        
+        /// <summary>
         /// btnSave control.
         /// </summary>
         /// <remarks>
