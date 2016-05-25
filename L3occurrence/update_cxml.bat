7f668ce5a87dd48afbc1dd2edb4e20b9fcac7373
George Birbilis <birbilis@zoomicon.com>
2016-05-25 03:06:53 +0300

    - Fixed update_cxml.bat for L3occurrences

--- L3occurrence/update_cxml.bat
+++ L3occurrence/update_cxml.bat
@@ -7,12 +7,12 @@ goto :EOF
 
 :process
 ECHO ---- Updating collection and DeepZoom assets
-c:\programs\pauthor\pauthor.exe /source cxml L3occurences.cxml /html-template template.html /target deepzoom ..\collection\L3occurences.cxml && (goto OK) || (goto Fail)
+c:\programs\pauthor\pauthor.exe /source cxml L3occurrences.cxml /html-template template.html /target deepzoom ..\collection\L3occurrences.cxml && (goto OK) || (goto Fail)
 goto :EOF
 
 :OK
 echo OK
-::start http://gallery.clipflair.net/L3occurences
+::start http://gallery.clipflair.net/L3occurrences
 goto :EOF
 
 :Fail
