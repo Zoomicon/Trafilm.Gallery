9861745cb448f8e619988059a63ea08c177bb675
George Birbilis <birbilis@zoomicon.com>
2016-06-29 22:09:28 +0300

    - Deleted temporary .html files generated by Pauthor that had remained as left-overs for some reason - update_cxml.bat files now delete old tiles to regenerate them (since Pauthor doesn't seem to always detect image url changes correctly) - regenerated galleries

--- L3TTinstance/update_cxml.bat
+++ L3TTinstance/update_cxml.bat
@@ -1,4 +1,4 @@
-:: Version: 20160525
+:: Version: 20160529
 :: Author: George Birbilis (http://zoomicon.com)
 
 @echo off
@@ -6,6 +6,9 @@ call :process > update_cxml.log
 goto :EOF
 
 :process
+ECHO ---- Deleting old DeepZoom assets
+rd /s/q ..\collection\L3TTinstances_deepzoom
+
 ECHO ---- Updating collection and DeepZoom assets
 c:\programs\pauthor\pauthor.exe /source cxml L3TTinstances.cxml /html-template template.html /target deepzoom ..\collection\L3TTinstances.cxml && (goto OK) || (goto Fail)
 goto :EOF
