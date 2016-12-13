a1a17e23395debc4d75c9a9b1edbe2e896f4c0ac
George Birbilis <birbilis@zoomicon.com>
2016-12-13 13:03:15 +0200

    - Gallery Updating (deepzoom tile and cxml generation via pauthor tool) scripts moved to UPDATING folder and rewritten for less offline time and better error logging

---
+++ UPDATING/update_L3TTinstances.bat
@@ -0,0 +1,46 @@
+:: Version: 20161213
+:: Author: George Birbilis (http://zoomicon.com)
+
+@echo off
+call :process 2>&1 > update_L3TTinstances.log
+goto :EOF
+
+:process
+setlocal
+set PAUTHOR=c:\programs\pauthor\pauthor.exe
+set SOURCE=L3TTinstance
+set COLLECTION=L3TTinstances
+
+ECHO ---- Deleting old temp folder (in case it exists from previous failed attempt)
+rd /s/q ..\collection\%COLLECTION%_TMP 2>&1
+
+ECHO ---- Generating collection and DeepZoom assets at temp folder
+cd ..\%SOURCE%
+%PAUTHOR% /source cxml %COLLECTION%.cxml /html-template template.html /target deepzoom ..\collection\%COLLECTION%_TMP\%COLLECTION%.cxml && (goto OK) || (goto Fail)
+
+:: control flow should never reach this, but manually terminating (jumping to implicit end-of-file marker) just in case
+goto :EOF
+
+:OK
+ECHO ---- Deleting old DeepZoom assets (to release from IIS)
+rd /s/q ..\collection\%COLLECTION%_deepzoom 2>&1
+
+ECHO --- Copying DeepZoom assets from temp folder, merging (overwriting already existing old ones)
+move /Y ..\collection\%COLLECTION%_TMP\%COLLECTION%_deepzoom ..\collection 2>&1
+
+ECHO --- Copying CXML file from temp folder, overwriting old one
+move /Y ..\collection\%COLLECTION%_TMP\%COLLECTION%.cxml ..\collection 2>&1
+
+
+ECHO --- Removing empty temp folder
+:: not using /s/q parameters to see if indeed all contents were moved from the temp folder succesfully
+rd ..\collection\%COLLECTION%_tmp 2>&1
+
+echo OK
+::start http://gallery.trafilm.net/
+goto :EOF
+
+:Fail
+:: not deleting temp folder (will delete at next run) that remained for easier troubleshooting of failed attempt
+echo FAILED
+goto :EOF
