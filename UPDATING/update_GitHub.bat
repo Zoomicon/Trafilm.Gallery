7ab08179b74805f5e616825800be2fc93ee7d7f8
George Birbilis <birbilis@zoomicon.com>
2016-12-15 12:35:17 +0200

    Metadata updates (auto commit) 2>&1 || (goto Fail)

--- UPDATING/update_GitHub.bat
+++ UPDATING/update_GitHub.bat
@@ -9,12 +9,15 @@ goto :EOF
 
 ECHO ---- Staging changes for addition to local repository
 git add . 2>&1 || (goto Fail)
+ECHO.
 
 ECHO ---- Commiting staged changes to local repository
 git commit -m "Metadata updates (auto commit) 2>&1 || (goto Fail)
+ECHO.
 
 ECHO ---- Pushing commited changes to remote repository
 git push origin master 2>&1 || (goto Fail)
+ECHO.
 
 :OK
 ECHO OK
